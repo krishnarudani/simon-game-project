237a557d025f3573d15c9e08e515e70f4d221c0b
krishnarudani <krishnarudani062@gmail.com>
2021-01-18 17:02:43 +0530

    add randome color press to blow sound and animatepress

--- game.js
+++ game.js
@@ -1,22 +1,20 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
-
 var userClickedPattern = [];
 
 $(".btn").click(function () {
   var userChosenColour = $(this).attr("id");
-
   userClickedPattern.push(userChosenColour);
 
   playSound(userChosenColour);
+
+  animatePress(userChosenColour);
 });
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
-
   var randomChosenColour = buttonColours[randomNumber];
-
   gamePattern.push(randomChosenColour);
 
   $("#" + randomChosenColour)
@@ -26,10 +24,13 @@ function nextSequence() {
 
   playSound(randomChosenColour);
 }
+
 function playSound(name) {
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
+
+//1. Create a new function called animatePress(), it should take a single input parameter called currentColour.
 function animatePress(currentColor) {
   //2. Use jQuery to add this pressed class to the button that gets clicked inside animatePress().
   $("#" + currentColor).addClass("pressed");
